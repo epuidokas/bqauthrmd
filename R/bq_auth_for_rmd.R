141b311e472ea6eb662c45542bf9ea57ae044f75
Eric Puidokas <eric@puidokas.com>
2019-08-01 08:55:54 -0400

    First commit

---
+++ R/bq_auth_for_rmd.R
@@ -0,0 +1,14 @@
+library("bigrquery")
+
+bq_auth_for_rmd <- function(){
+  if (!interactive() && Sys.getenv("GOOGLE_AUTH_USER") != "") {
+    message("Authenticating as ", Sys.getenv("GOOGLE_AUTH_USER"))
+    bq_auth(Sys.getenv("GOOGLE_AUTH_USER"))
+  } else {
+    bq_auth()
+    message("********************************************")
+    message("To automatically authenticate with Google as ", bq_user(), ", run the following in Terminal:")
+    message("printf \"\\nGOOGLE_AUTH_USER=", bq_user(), "\" >> ~/.REnviron")
+    message("********************************************")
+  }
+}
