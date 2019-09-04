a4f5640b7bbfc1850d235b06a0fbf5dd3d8f713a
Eric Puidokas <eric@puidokas.com>
2019-09-04 10:22:41 -0400

    Remove scopes

--- R/bq_auth_for_rmd.R
+++ R/bq_auth_for_rmd.R
@@ -3,12 +3,9 @@ library("bigrquery")
 bq_auth_for_rmd <- function(){
   if (!interactive() && Sys.getenv("GOOGLE_AUTH_USER") != "") {
     message("Authenticating as ", Sys.getenv("GOOGLE_AUTH_USER"))
-    bq_auth(email = Sys.getenv("GOOGLE_AUTH_USER"),
-            scopes = c("https://www.googleapis.com/auth/cloud-platform.read-only",
-                       "https://www.googleapis.com/auth/devstorage.read_only"))
+    bq_auth(email = Sys.getenv("GOOGLE_AUTH_USER"))
   } else {
-    bq_auth(scopes = c("https://www.googleapis.com/auth/cloud-platform.read-only",
-                       "https://www.googleapis.com/auth/devstorage.read_only"))
+    bq_auth()
     message("********************************************")
     message("To automatically authenticate with Google as ", bq_user(), ", run the following in Terminal:")
     message("printf \"\\nGOOGLE_AUTH_USER=", bq_user(), "\" >> ~/.REnviron")
